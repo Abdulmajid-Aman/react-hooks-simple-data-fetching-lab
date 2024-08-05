2f46c6c1415276d648cd425ae7299c4a274a58d6
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-08-05 19:49:17 +0300

    Fething lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
-// create your App component here
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [image, setImage] = useState("");
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(res => res.json())
+      .then(data => {
+        setImage(data.message);
+        setIsLoaded(true);
+      });
+  }, []);
+
+  return (
+    <div>
+      {isLoaded ? <img src={image} alt="A Random Dog" /> : <p>Loading...</p>}
+    </div>
+  );
+}
+
+export default App;
